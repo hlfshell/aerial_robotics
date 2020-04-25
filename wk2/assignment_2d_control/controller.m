7ef9331f6fb31968d5f0ccddbad1e28b870f0ee4
Keith Chester <kchester@gmail.com>
2020-04-25 15:10:47 -0700

    feature: Working finished assignment

--- wk2/assignment_2d_control/controller.m
+++ wk2/assignment_2d_control/controller.m
@@ -14,10 +14,67 @@ function [ u1, u2 ] = controller(~, state, des_state, params)
 %   Using these current and desired states, you have to compute the desired
 %   controls
 
-u1 = 0;
-u2 = 0;
+% Robot Parameters:
+% params.gravity - 9.8100
+% params.mass - weight of quadrotor - 0.1800
+% params.Ixx - Moment of inertia - 2.5e-04
+% params.arm_length - length of quadrotor arm - 0.060
+% params.minF - minimum thrust force - 0
+% params.maxF - maximum thrust force - 3.5316
 
-% FILL IN YOUR CODE HERE
+% State des_state
+% pos - 2x1 [] double - [y_des, z_des]
+% vel - 2x1 [] double - [y_d_des, z_d_des]
+% acc - 2x1 [] double - [y_d_des, z_d_des]
+
+% State state
+% pos - 2x1 [] double - [y, z]
+% rot - double - phi
+% vel - 2x1 [] double - [y_d, z_d]
+% omega - double - phi_d
+
+y = state.pos(1);
+y_des = des_state.pos(1);
+y_d = state.vel(1);
+y_d_des = des_state.vel(1);
+y_dd_des = des_state.acc(1);
+
+z = state.pos(2);
+z_des = des_state.pos(2);
+z_d = state.vel(2);
+z_d_des = des_state.vel(2);
+z_dd_des = des_state.acc(2);
+
+phi = state.rot;
+phi_d = state.omega;
+
+% CONSTANT GAINS
+% U1 GAINS
+K_dy = 5; % derivative Y gain
+K_py = 20; % proportional Y gain
+% U2 GAINS
+K_dphi = 50; % derivative Phi gain
+K_pphi = 1000; % proportional Phi gain
+% PHI GAINS
+K_dz = 20; % derivative Z gain
+K_pz = 80; % proportonal Z gain
+% 
+% K_dy = 5; % derivative Y gain Kv_y
+% K_py = 40; % proportional Y gain Kp_y
+% K_dphi = 50; % derivative Phi gain Kv_z
+% K_pphi = 1000; % proportional Phi gain Kp_z
+% K_dz = 20; % derivative Z gain Kv_z
+% K_pz = 130; % proportonal Z gain Kp_z
+
+%phi_c = -(1/g) (y_dd_des + K_dy (y_d_des - y_d) + K_py(y_des - y))
+phi_c = (-1.0/params.gravity) * (y_dd_des + ( K_dy * (y_d_des - y_d))  + ( K_py * (y_des - y)) );
+phi_d_c = 0;
+phi_dd_c = 0;
+
+% u2 = Ixx(phi_dd_c + K_pphi(phi_c - phi) + K_dphi(phi_d_c-phi_d))
+u2 = params.Ixx * (phi_dd_c + ( K_pphi * (phi_c - phi)) + ( K_dphi * (phi_d_c - phi_d)) );
+
+% u1 = m(g + z_dd_des + K_dz(z_d_des - z_d) + K_pz(z_des - z))
+u1 = params.mass * (params.gravity + z_dd_des + (K_dz * (z_d_des - z_d)) + (K_pz * (z_des - z)));
 
 end
-
--- wk2/assignment_2d_control/runsim.m
+++ wk2/assignment_2d_control/runsim.m
@@ -7,7 +7,8 @@ addpath('trajectories');
 controlhandle = @controller;
 
 % Choose which trajectory you want to test with
-trajhandle = @traj_line;
-% trajhandle = @traj_sine;
+% trajhandle = @traj_line;
+trajhandle = @traj_sine;
+% trajhandle = @traj_diamond;
 
 [t, state] = simulation_2d(controlhandle, trajhandle);
